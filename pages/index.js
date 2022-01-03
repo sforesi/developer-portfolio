d4b2519263d29ef986df9eb9347bb0f512b58b36
sforesi <sebastiano.foresi@gmail.com>
2022-01-02 22:26:44 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+const Navigation = dynamic(() => import("../components/Navigation"));
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Experience = dynamic(() => import("../containers/Experience"));
+const Projects = dynamic(() => import("../containers/Projects"));
+const GithubProfileCard = dynamic(() =>
+	import("../components/GithubProfileCard")
+);
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+
+export default function Home({ githubProfileData }) {
+	return (
+		<div>
+			<SEO
+				data={{
+					title: "Sebastiano Foresi",
+					description:
+						"A passionate Full Stack Web Developer and Mobile App Developer.",
+					image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+					url: "https://developer-portfolio-1Sebastiano100.vercel.app",
+					keywords: [
+						"Sebastiano",
+						"Sebastiano Foresi",
+						"@1Sebastiano100",
+						"1Sebastiano100",
+						"Portfolio",
+						"Sebastiano Portfolio ",
+						"Sebastiano Foresi Portfolio",
+						"web developer",
+						"full stack",
+						"full stack web developer",
+						"mobile app developer",
+						"android developer",
+						"django",
+						"flask",
+						"django rest framework",
+						"nodejs ",
+						"expressjs",
+						"reactjs ",
+						"contextapi",
+						"redux",
+						"flutter",
+					],
+				}}
+			/>
+			<Navigation />
+			<Greetings />
+			<Projects />
+			<Experience />
+			<GithubProfileCard prof={githubProfileData} />
+		</div>
+	);
+}
+
+Home.prototype = {
+	githubProfileData: PropTypes.object.isRequired,
+};
+
+export async function getStaticProps(_) {
+	const githubProfileData = await fetch(
+		`https://api.github.com/users/${openSource.githubUserName}`
+	).then((res) => res.json());
+
+	return {
+		props: { githubProfileData },
+	};
+}
---
+++ portfolio.js
@@ -0,0 +1,135 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Sebastino Foresi",
+	title: "About",
+	description:
+		"Enthusiastic multilingual Software Engineer eager to learn new technologies and work through bugs with commitment and collaboration.",
+	resumeLink: "https://docs.google.com/document/d/11rulHEME3Yuz0eG4yWPWW9s7uMra7Anc/edit?usp=drive_web&ouid=101577557596778446032&rtpof=true",
+};
+
+export const openSource = {
+	githubUserName: "sforesi",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	github: "https://github.com/sforesi",
+	linkedin: "https://www.linkedin.com/in/sebastiano-foresi/",
+	instagram: "https://www.instagram.com/1hanzla100",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		" Build and conceptualized multiple mobile and web-applications using: HTML, CSS, JavaScript, Bootstrap, Express, Node.js, React, Github, MongoDB, Python, Django and more.",
+
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+	],
+};
+
+
+
+export const educationInfo = [
+	{
+		schoolName: "General Assembly",
+		subHeader: "Software Engineering Immersive Course",
+		desc: "Completed over 500 hours of Software Engineering training where I learned skills needed to be a full-stack software developer",
+	},
+	{
+		schoolName: "Sarah Lawrence College",
+		subHeader: "Bachelors of Arts",
+		desc: "Focused in Film and Television Production.",
+	},
+	
+];
+
+export const experience = [
+	{
+		role: "Producer's Assistant",
+		company: "Slow Motion Riots",
+		companylogo: "/img/icons/common/github.svg",
+		date: "Jan 2020 – April 2021",
+		desc: "Pitched, researched, and proofread scripts for Producers. Led collaboration with Writers to make projects more appealing to Producers. Proposed and vetted projects with upcoming Writers ",
+	},
+	{
+		role: "Producer's Assistant",
+		company: "NBCUniversal",
+		companylogo: "/img/icons/common/google.svg",
+		date: "Jan 2020 – March 2020",
+		desc: "Revised and edited script with Director and Writers. Planned and coordinated shooting for the script. Worked directly with Producer to schedule meetings with UPM and other managing crew.",
+	},
+	{
+		role: "Production Assistant",
+		company: "Apple TV",
+		companylogo: "/img/icons/common/airbnbLogo.png",
+		date: "May 2019 – Jan 2020",
+		desc: "Set up and formatted pilot Table Read at Apple Headquarters. Negotiated rates for hundreds of hotel bookings for cast and crew. Met with cast members to ensure their contracts were signed, archived, and logged.",
+	},
+];
+
+export const projects = [
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://example.com",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1100",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+		link: "https://example.com",
+	},
+];
+
